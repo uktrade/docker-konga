5354e0146aa159f400579e85d6da75d1a0642eac
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-10 16:21:14 +0100

    run migrations on container start

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export DB_URI="$DATABASE_URL"
-export DB_PG_SCHEMA="postgres"
+export DB_PG_SCHEMA=postgres
 export HOST=0.0.0.0
+
+node ./bin/konga.js  prepare --adapter postgres $DB_URI
 
 exec /app/_start.sh $@ 
