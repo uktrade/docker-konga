95c7923f397832804aadf77b4f6bd5009de89447
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-10 16:39:21 +0100

    debug migrations

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ export DB_URI="$DATABASE_URL"
 export DB_PG_SCHEMA=postgres
 export HOST=0.0.0.0
 
-node ./bin/konga.js  prepare --adapter postgres $DB_URI
+echo "Running migrations...."
+node ./bin/konga.js  prepare --adapter postgres $DATABASE_URL 
+echo "End of migrations"
 
 exec /app/_start.sh $@ 
