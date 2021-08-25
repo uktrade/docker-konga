1219f52654aacb888c44af20627bb9d754106ef2
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-08-25 18:00:03 +0100

    add DB_ADAPTER to start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export DB_URI="$DATABASE_URL?ssl=true"
+export DB_ADAPTER=postgres
 export DB_PG_SCHEMA=postgres
+export DB_URI="$DATABASE_URL?ssl=true"
 export HOST=0.0.0.0
 export NODE_ENV=${NODE_ENV:-production}
 
